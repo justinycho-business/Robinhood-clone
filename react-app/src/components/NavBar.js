26ad36515ca865dc4b1d9aa3dde239bd83df47a0
Justin <justinycho.business@gmail.com>
2021-07-15 16:42:26 -0700

    deleted protected route from home

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
-
+import './styles/NavBar.css';
 const NavBar = () => {
   return (
     <nav>
