cfef1ff37ff0df09866113749c0755a00b72d7d1
josh-willy91 <jwilly91812@yahoo.com>
2021-07-18 17:49:13 -0400

    added css to dashboard page

--- react-app/src/components/Dashboard.js
+++ react-app/src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-
+import './Dashboard.css';
 
 function Dashboard() {
     const dispatch = useDispatch()
@@ -15,16 +15,23 @@ function Dashboard() {
     }
 
     return (
-        <div>
-            <div>
+        <div class='wrapper'>
+            <div className='portfolioDiv'>
                 <h1>Dashboard</h1>
-            </div>
-            <div>
                 <h1>Add Total Portfolio Value Monday {user.username}</h1>
                 <h3>Add a daily percent change {user.username}</h3>
-                <h1>Graph goes here</h1>
             </div>
-            <div>
+            <div className='graph'>
+                <div>Graph Goes Here</div>
+                <button>1D</button>
+                <button>1W</button>
+                <button>1M</button>
+                <button>3M</button>
+                <button>1Y</button>
+                <button>5Y</button>
+                <button>All</button>
+            </div>
+            <div className='addFundsDiv'>
                 <h3>Buying Power</h3>
                 <h3>Add the available cash Monday {user.username}</h3>
                 <label>Add Funds</label>
@@ -36,8 +43,31 @@ function Dashboard() {
                 />
                 <button type="submit">Submit Funds</button>
             </div>
-            <div>
+            <div className='watchlistDiv'>
                 <h1>Watchlist</h1>
+                <ul className='watchlistUl'>
+                    <li className='watchlistLi'>
+                        <p className='ticker'>AAPL</p>
+                        <p className='shares'>70 Shares</p>
+                        <p className='lilGraph'>Small Graph</p>
+                        <p className='price'>Price</p>
+                        <p className='percent'>Percent Change</p>
+                    </li>
+                    <li className='watchlistLi'>
+                        <p className='ticker'>BA</p>
+                        <p className='shares'>70 Shares</p>
+                        <p className='lilGraph'>Small Graph</p>
+                        <p className='price'>$438.34</p>
+                        <p className='percent'>-4.11%</p>
+                    </li>
+                    <li className='watchlistLi'>
+                        <p className='ticker'>TSLA</p>
+                        <p className='shares'>70 Shares</p>
+                        <p className='lilGraph'>Small Graph</p>
+                        <p className='price'>$652.09</p>
+                        <p className='percent'>2.38%</p>
+                    </li>
+                </ul>
             </div>
         </div>
     )
