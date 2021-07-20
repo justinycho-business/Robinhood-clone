9278f0582da06879fc3418e7c9adb13d58ff70af
Ebrahim Tayara <ebrahim@tayara.net>
2021-07-19 21:58:50 -0700

    matched  most of the styling

--- react-app/src/components/AboutUs.js
+++ react-app/src/components/AboutUs.js
@@ -9,30 +9,44 @@ const AboutUs = () => {
         </div>
         <div className="about_us_outer_container">
             <div className="about_us_inner_container">
+                <div className="about_us_container">
                     <div className="each_person">
                         <img src="/pics/Joshua.png" className="person_pic"></img>
                             <a href="https://github.com/josh-willy91" target="_blank">
                               Joshua Williams
                             </a>
+                    <div className="person_desc">
+                        <h2>Awesome guy.</h2>
+                    </div>
                     </div>
                     <div className="each_person">
                         <img src="/pics/Gary.png" className="person_pic"></img>
                             <a href="https://github.com/babypwant" target="_blank">
                               Gary Rios
                             </a>
+                    <div className="person_desc">
+                        <h2>Favorite color is green.</h2>
+                    </div>
                     </div>
                     <div className="each_person">
                         <img src="/pics/Eb.png" className="person_pic"></img>
                             <a href="https://github.com/ebtayara" target="_blank">
                               Ebrahim Tayara
                             </a>
+                    <div className="person_desc">
+                        <h2>Sucks at CSS.</h2>
+                    </div>
                     </div>
                     <div className="each_person">
                         <img src="/pics/Justin.png" className="person_pic"></img>
                             <a href="https://github.com/justinycho-business" target="_blank">
                               Justin Cho
                             </a>
+                    <div className="person_desc">
+                        <h2>Enjoys the finer things in life.</h2>
                     </div>
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
