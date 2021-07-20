bce1d50032e1b3e65a12979d0b3255afe21c800d
Ebrahim Tayara <ebrahim@tayara.net>
2021-07-19 20:57:07 -0700

    cssing about us - adding pics

--- react-app/src/components/AboutUs.js
+++ react-app/src/components/AboutUs.js
@@ -5,29 +5,31 @@ const AboutUs = () => {
   return(
     <div>
         <div className="title_container">
-          <h1 className="title">We want to help you help yourself.</h1>
+            <h1 className="title">We want to help you help yourself.</h1>
         </div>
-        <div className="about_us_container">
-            <a href="https://github.com/josh-willy91" target="_blank">
-                <div className="each_person">
-                    <img src="" className="person_pic"></img>
+        <div className="about_us_outer_container">
+            <div className="about_us_inner_container">
+                <a href="https://github.com/josh-willy91" target="_blank">
+                    <div className="each_person">
+                        <img src="/public/pics/Joshua.png" className="person_pic"></img>
+                    </div>
+                </a>
+                <a href="https://github.com/babypwant" target="_blank">
+                    <div className="each_person">
+                        <img src="/public/pics/Gary.png" className="person_pic"></img>
+                    </div>
+                </a>
+                <a href="https://github.com/ebtayara" target="_blank">
+                    <div className="each_person">
+                        <img src="/public/pics/Eb.png" className="person_pic"></img>
+                    </div>
+                </a>
+                <a href="https://github.com/justinycho-business" target="_blank">
+                    <div className="each_person">
+                        <img src="/public/pics/Justin.png" className="person_pic"></img>
+                    </div>
+                </a>
                 </div>
-            </a>
-            <a href="https://github.com/babypwant" target="_blank">
-                <div className="each_person">
-                    <img src="" className="person_pic"></img>
-                </div>
-            </a>
-            <a href="https://github.com/ebtayara" target="_blank" id="github_link">
-                <div className="each_person">
-                    <img src="" className="person_pic"></img>
-                </div>
-            </a>
-            <a href="https://github.com/justinycho-business" target="_blank">
-                <div className="each_person">
-                    <img src="" className="person_pic"></img>
-                </div>
-            </a>
             </div>
         </div>
   )
