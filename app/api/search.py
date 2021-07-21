b990c4b2ef4b4b1a361732d48d47e93c4cc28294
Ebrahim Tayara <ebrahim@tayara.net>
2021-07-21 09:23:20 -0700

    creating api route for search feature

---
+++ app/api/search.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+from app.models import Company
+
+#need to query for/return tickr
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
