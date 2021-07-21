b041576b1737541a663ee35555503556b744a138
Ebrahim Tayara <ebrahim@tayara.net>
2021-07-21 13:07:20 -0700

    redoing routes

--- app/api/search.py
+++ app/api/search.py
@@ -13,24 +13,27 @@ apikey2 = os.environ.get('API_2_FIN')
 
 #routes
 #need to query for/return tickers
-@search_routes.route('/')
-@login_required
-def users():
-    tickers = Company.query.all()
-    return {'tickers': [tickers.to_dict() for ticker in tickers]}
 
-@search_routes.route('/<int:id>')
-@login_required
-def ticker(id):
-    ticker = Company.query.get(id)
-    return ticker.to_dict()
 
-def get_ticker():
-    result = []
-    tickers = Company.query.all()
-    ticker_array = [ticker.to_dict() for ticker in tickers]
-    for ticker in ticker_array:
-        res = requests.get(f'https://financialmodelingprep.com/api/v3/quote-short/{ticker}?apikey={apikey2}')
-        jsonData = res.json()
-        result.append(jsonData)
-    return result
+
+# @search_routes.route('/')
+# @login_required
+# def tickers():
+#     tickers = Company.query.all()
+#     return {'tickers': [tickers.to_dict() for ticker in tickers]}
+
+# @search_routes.route('/<int:id>')
+# @login_required
+# def ticker(id):
+#     ticker = Company.query.get(id)
+#     return ticker.to_dict()
+
+# def get_ticker():
+#     result = []
+#     tickers = Company.query.all()
+#     ticker_array = [ticker.to_dict() for ticker in tickers]
+#     for ticker in ticker_array:
+#         res = requests.get(f'https://financialmodelingprep.com/api/v3/quote-short/{ticker}?apikey={apikey2}')
+#         jsonData = res.json()
+#         result.append(jsonData)
+#     return result
