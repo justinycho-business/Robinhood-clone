33e8bae2871900d85a778c402993eead7b4d2779
Ebrahim Tayara <ebrahim@tayara.net>
2021-07-23 16:00:58 -0700

    re-styled footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -4,12 +4,14 @@ import './styles/Footer.css';
 
 const Footer = () => {
   return(
-    <div className="footer">
-          <div>
-          <NavLink to='/about-us' className='about_us' exact={true}>
-            About us
-          </NavLink>
-          </div>
+    <div className="footer_container">
+      <div className="footer">
+            <div>
+            <NavLink to='/about-us' className='about_us' exact={true}>
+              About us
+            </NavLink>
+            </div>
+      </div>
     </div>
   )
 }
