f0138dd668e4dd0dae258806c936d8ccac9b85a8
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-08-24 11:23:09 -0700

    reworking about us page and bios

--- react-app/src/components/AboutUs.js
+++ react-app/src/components/AboutUs.js
@@ -2,74 +2,79 @@ import React from 'react';
 import './styles/AboutUs.css';
 
 const AboutUs = () => {
-  return(
-    <div>
-        <div className="title_container">
-            <h1 className="title">We want to help you help yourself.</h1>
-        </div>
-        <div className="about_us_outer_container">
-            <div className="about_us_inner_container">
-                <div className="about_us_container">
-                <div className="second_title_container">
-                  <h2 className="second_title">Meet our team</h2>
-                </div>
-                    <div className="each_person">
-                        <img src="/pics/Joshua.png" className="person_pic"></img>
-                            <a href="https://github.com/josh-willy91" target="_blank">
-                              <div className = "github_icon_ container">
-                                <div className = "github_icon">
-                                  <i class="fab fa-github"></i>
-                              </div>
-                                </div>
-                            </a>
-                    <div className="person_desc">
-                        <h3>Dreamt about this moment.</h3>
-                    </div>
-                    </div>
-                    <div className="each_person">
-                        <img src="/pics/Gary.png" className="person_pic"></img>
+    return (
+        <div>
+            <div className="title_container">
+                <h1 className="title-about-us">Meet the team</h1>
+            </div>
+            <div className="about_us_outer_container">
+                <div className="about_us_inner_container">
+                    <div className="about_us_container">
+                        <div className="each_person">
+                            <img src="/pics/Gary.png" className="person_pic"></img>
                             <a href="https://github.com/babypwant" target="_blank">
-                            <div className = "github_icon_ container">
-                                <div className = "github_icon">
-                                  <i class="fab fa-github"></i>
-                              </div>
+                                <div className="github_icon_ container">
+                                    <div className="github_icon">
+                                        <i class="fab fa-github"></i>
+                                    </div>
                                 </div>
                             </a>
-                    <div className="person_desc">
-                        <h3>Favorite color is green.</h3>
-                    </div>
-                    </div>
-                    <div className="each_person">
-                        <img src="/pics/Eb.png" className="person_pic"></img>
-                            <a href="https://github.com/ebtayara" target="_blank">
-                            <div className = "github_icon_ container">
-                                <div className = "github_icon">
-                                  <i class="fab fa-github"></i>
-                              </div>
-                                </div>
-                            </a>
-                    <div className="person_desc">
-                        <h3>Doggo has no boundaries.</h3>
-                    </div>
-                    </div>
-                    <div className="each_person">
-                        <img src="/pics/Justin.png" className="person_pic"></img>
+                            <div className="person_desc">
+                                <h3>Gary Rios</h3>
+                                <h2>
+                                    eSports Player-manager / Video editor / Software Engineer
+                                </h2>
+
+                            </div>
+                        </div>
+                        <div className="each_person">
+                            <img src="/pics/Justin.png" className="person_pic"></img>
                             <a href="https://github.com/justinycho-business" target="_blank">
-                            <div className = "github_icon_ container">
-                                <div className = "github_icon">
-                                  <i class="fab fa-github"></i>
-                              </div>
+                                <div className="github_icon_ container">
+                                    <div className="github_icon">
+                                        <i class="fab fa-github"></i>
+                                    </div>
                                 </div>
                             </a>
-                    <div className="person_desc">
-                        <h3>Enjoys the finer things in life.</h3>
+                            <div className="person_desc">
+                                <h3>Justin Cho.</h3>
+                                <h2>
+                                    "You can always improve your mind, body, or spirit. There is no time for complacency." -Justin Cho | I seek challenges and learn every day. Go higher, always.
+                                </h2>
+                            </div>
+                        </div>
+                        <div className="each_person">
+                            <img src="/pics/Joshua.png" className="person_pic"></img>
+                            <a href="https://github.com/josh-willy91" target="_blank">
+                                <div className="github_icon_ container">
+                                    <div className="github_icon">
+                                        <i class="fab fa-github"></i>
+                                    </div>
+                                </div>
+                            </a>
+                            <div className="person_desc">
+                                <h3>Josh Williams</h3>
+                            </div>
+                        </div>
+                        <div className="each_person">
+                            <img src="/pics/Eb.png" className="person_pic"></img>
+                            <a href="https://github.com/ebtayara" target="_blank">
+                                <div className="github_icon_ container">
+                                    <div className="github_icon">
+                                        <i class="fab fa-github"></i>
+                                    </div>
+                                </div>
+                            </a>
+                            <div className="person_desc">
+                                <h3>Ebrahim Tayara</h3>
+                            </div>
+                        </div>
+
                     </div>
-                    </div>
-                  </div>
                 </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default AboutUs
